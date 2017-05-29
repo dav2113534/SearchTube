150353c33ec02b73e6df6c8daa79949bd2a0ccfe
dav2113534 <dpalacios03081991@gmail.com>
2017-05-28 17:50:55 -0700

    added zip code search bar

---
+++ main.js
@@ -0,0 +1,15 @@
+var ticketmasterUrl = 'https://app.ticketmaster.com/discovery/v2/'
+
+//add a zip code search bar to find events near a 50 mile
+//radius 
+
+/* 
+Keep each comedian to one genre(most popular genre)
+use to split strings 
+
+json file format
+[
+  { genre: "Spoof", comedians: ["Mel Brooks", "French and Saunders"] }
+]
+"Richard Pryor, Jeff Foxworthy, Gorge Lopez, Jerrod Guillory, Bill Engvall, Gabriel Iglesias".split(',').map((c) => c.trim())
+*/
