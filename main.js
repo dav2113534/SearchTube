8953c71123c457076fc102398abf8f9419d15c36
dav2113534 <dpalacios03081991@gmail.com>
2017-06-06 23:03:45 -0700

    added submit button

--- main.js
+++ main.js
@@ -5,25 +5,56 @@ function getData(comedian) {
         apikey: '6m1NAjVcdP4FZrAj7JShG7KDuGN6FlAN',
         keyword: comedian,
     }
-    console.log(comedian)
 
-    $.getJSON(ticketmasterUrl, getJson, findMatch, comedian)
+    $.getJSON(ticketmasterUrl, getJson, findMatch)
 }
-
-
-
 
 function findMatch(data) {
     console.log(data)
-    var next = state.getComedianPool[0] + 1
-    if (data !== state.length - 1) {
+    var next = state.getComedianPool
+    if (data !== state.getComedianPool.length) {
         return next
     }
 }
 
-// Your goal is to display list of related comedians(based on genre) 
-//for comedian they have selected. If you complete it go ahead 
-//and try to call Ticketmaster api with selected comedian.
+
+//Complete gathers up the arrays 
+function complete() {
+    var comedians =
+        state.genres.map(function (x) {
+            return x.comedians;
+        })
+    //This will combine every array into a single array
+    //Refer to this repl https://repl.it/I0hd/0
+    return Array.prototype.concat.apply([], comedians);
+}
+
+
+
+function renderRelatedComedians(comedians) {
+    comedians = comedians.join(", ")
+        .split("  ");
+    $('.sameGenre').html(comedians);
+}
+
+function onComedianSelected(e, selected) {
+    var relatedComedians = state.getRelatedComedians(selected.item.value)
+    //pool is a pool of functions 
+    state.getComedianPool = [];
+    state.getComedianPool.push(selected.item.value);
+    state.getComedianPool = state.getComedianPool.concat(relatedComedians);
+
+
+    renderRelatedComedians(relatedComedians);
+    // console.log(state.getRelatedComedians(selected.item.value));
+}
+// 
+function render() {
+    $('.searchTerm').autocomplete({
+        source: complete(),
+        select: onComedianSelected
+    })
+}
 
 
 function getRandomColor() {
@@ -41,57 +72,5 @@ function setRandomColor() {
 
 setRandomColor();
 
-
-
-//Complete gathers up the arrays 
-function complete() {
-    var comedians =
-        state.genres.map(function (x) {
-            return x.comedians;
-        })
-    //This will combine every array into a single array
-    //Refer to this repl https://repl.it/I0hd/0
-    return Array.prototype.concat.apply([], comedians);
-}
-
-
-function getSearchTerm() {
-    return $('.searchTerm').val();
-
-}
-
-
-function renderRelatedComedians(comedians) {
-    comedians = comedians.join(", ")
-        .split("  ");
-    $('.sameGenre').html(comedians);
-}
-
-function getDataForComedian(comedian) {
-    return function () {
-        console.log(comedian)
-        return getData(comedian)
-    };
-}
-// 
-function render() {
-    $('.searchTerm').autocomplete({
-        source: complete(),
-        select: function (e, selected) {
-            var relatedComedians = state.getRelatedComedians(selected.item.value)
-            //pool is a pool of functions 
-            state.getComedianPool = [];
-            state.getComedianPool.push(getDataForComedian(selected.item.value));
-            state.getComedianPool = state.getComedianPool.concat(
-                relatedComedians.map(function (related) {
-                    return getDataForComedian(related)
-                }));
-
-
-            renderRelatedComedians(relatedComedians);
-            // console.log(state.getRelatedComedians(selected.item.value));
-        }
-    })
-}
 
 render();
