b20320fd7ace8255134816e7abd05bfdea331569
dav2113534 <dpalacios03081991@gmail.com>
2017-07-06 13:44:55 -0700

    added footer

--- app/js/main.js
+++ app/js/main.js
@@ -21,40 +21,6 @@ function saveEvents(data) {
     }
     render();
 }
-
-
-// function makeComediansClick() {
-// $('.sameGenre').click(function() {
-//     // Gets clicked on word (or selected text if text is selected)
-//     var t = '';
-//     if (window.getSelection && (sel = window.getSelection()).modify) {
-//         // Webkit, Gecko
-//         var s = window.getSelection();
-//         if (s.isCollapsed) {
-//             s.modify('move', 'forward', 'character');
-//             s.modify('move', 'backward', 'word');
-//             s.modify('extend', 'forward', 'word');
-//             t = s.toString();
-//             s.modify('move', 'forward', 'character'); //clear selection
-//         }
-//         else {
-//             t = s.toString();
-//         }
-//     } else if ((sel = document.selection) && sel.type != "Control") {
-//         // IE 4+
-//         var textRange = sel.createRange();
-//         if (!textRange.text) {
-//             textRange.expand("word");
-//         }
-//         // Remove trailing spaces
-//         while (/\s$/.test(textRange.text)) {
-//             textRange.moveEnd("character", -1);
-//         }
-//         t = textRange.text;
-//     }
-//     alert(t);
-// });
-// }
 //Complete gathers up the arrays 
 function complete() {
     var comedians =
@@ -135,7 +101,7 @@ function renderMap(x) {
     const coordinates = lat + ',' + long;
     return '<img src="https://maps.google.com/maps/api/staticmap?center=' +
         coordinates +
-        '&zoom=12&size=400x300&sensor=false" style="width: 400px; height: 400px;" />'
+        '&zoom=12&size=400x300&sensor=false" style="width: 500px; height: 400px;" />'
 }
 
 
